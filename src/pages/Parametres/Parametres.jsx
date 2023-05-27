a1a98da905cd4a262a405023da0960a88fea5725
lando00 <orlandojulien2@gmail.com>
2023-05-28 01:55:12 +0200

    Pages parametres fini

--- src/pages/Parametres/Parametres.jsx
+++ src/pages/Parametres/Parametres.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Parametres.css';
 import TitrePage from '../../components/TitrePage/TitrePage';
+import InputForm from '../../components/InputForm/InputForm';
+import BtnVert from '../../components/Bouton/BtnVert';
+import BtnRouge from '../../components/Bouton/BtnRouge';
 
 const Parametres = () => {
   return (
 
     <div className='parametres'>
        <TitrePage titre="Paramètres" />
+       <ul className="sous-menu">
+          <li>
+            <NavLink to="/parametres">CHANGER MOT DE PASSE</NavLink>
+          </li>
+        </ul>
+        <div className='nouveau-clients'>
+          <div className="form">
+            <InputForm label="Ancien mot de passe" value="" />
+            <InputForm label="Nouveau mot de passe" value="" />
+            <InputForm label="Confirmer mot de passe" value="" />
+            <div className="btn-group">
+              <BtnRouge>Annuler</BtnRouge>
+              <BtnVert>Enregistrer</BtnVert>
+            </div>
+          </div>
+        </div>
     </div>
+
 
   )
 }
 
-export default Parametres
+export default Parametres 
